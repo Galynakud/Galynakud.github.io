93fb4b5387587b0ee8abee05fb7db07a13fec33f
Galyna Kud <Halyna.Kud.OI.2023@lpnu.ua>
2025-05-03 21:31:31 +0300

    Create App.js

---
+++ my-city-simulator/App.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import './styles/styles.css';
+
+import Header from './components/Header';
+import Construction from './components/Construction';
+import Resources from './components/Resources';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import Home from './components/Home';
+
+import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
+function AppWrapper() {
+  const [user, setUser] = useState(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    fetch("http://localhost:5000")
+    .then(response => response.json())  
+    .then(data => console.log(data.message));  
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const isLoginPage = location.pathname === '/';
+
+  return (
+    <div className="App">
+      {}
+      {user && !isLoginPage && <Header user={user} />}
+
+      <main>
+        
+        <Routes>
+          {}
+          <Route path="/" element={<Login />} />
+
+          {}
+          {user ? (
+            <>
+              <Route path="/home" element={<Home />} />
+              <Route path="/construction" element={<Construction />} />
+              <Route path="/resources" element={<Resources />} />
+              <Route path="*" element={<Navigate to="/home" />} />
+            </>
+          ) : (
+            <Route path="*" element={<Navigate to="/" />} />
+          )}
+        </Routes>
+      </main>
+
+      {}
+      {user && !isLoginPage && <Footer />}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppWrapper />
+    </Router>
+  );
+}
+
+export default App;
